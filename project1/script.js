b8cec2c22b32d591261ddd901a18eb26c9dfa74c
shahzaib332 <72549705+shahzaib332@users.noreply.github.com>
2021-06-18 18:50:54 +0500

    Update script.js
    added email verificatio with simple js

--- project1/script.js
+++ project1/script.js
@@ -20,7 +20,14 @@ function showSuccess(input){
     formControl.className = 'form-control success';
 
 }
+//function to check if email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());//copy from google
 
+
+
+}
 
 //event listner
 form.addEventListener('submit',function(e){
@@ -36,7 +43,11 @@ form.addEventListener('submit',function(e){
     if(email.value===''){
         showError(email,'email is required');
 
-    }else{
+    }else if(!isValidEmail(email.value)){
+        showError(email,'Email is ivalid');
+
+    }
+    else{
         showSuccess(email);
     }
     if(password.value===''){
